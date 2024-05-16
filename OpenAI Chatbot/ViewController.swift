4054bd244d738ee8a84ff5e943059232d9f892f4
Shivam Shishangia <shivams@arizona.edu>
2024-05-15 20:01:33 -0700

    Initial Project creation

---
+++ OpenAI Chatbot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenAI Chatbot
+//
+//  Created by Shivam Shishangia on 15/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
